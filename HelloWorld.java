0d41e4f19143d82c5bddee525d8e2d7bdadd7a75
langlang <1293261994@qq.com>
2019-07-26 10:11:19 +0800

    我也不知道改写些什么

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,9 @@ public class HelloWorld{
 	public static void main (Sting[] args){
 		system.out.println("傻子,忘了吧!");
 		system.out.println("傻子,别忘了!");
+		system.out.println("傻子,别忘了!");
+		system.out.println("傻子,别忘了!");
+		system.out.println("傻子,别忘了!");
+		system.out.println("傻子,别忘了!");
 	}
 }
